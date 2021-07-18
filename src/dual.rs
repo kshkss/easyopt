8418ff1d24e923a01844fd6b651d0d797a363ce4
Akishika Issui <kshkss@users.noreply.github.com>
2021-07-18 23:41:44 +0900

    Change implementation of dual
    * Use array to represent the dual part

--- src/dual.rs
+++ src/dual.rs
@@ -2,36 +2,31 @@ use ndarray::prelude::*;
 use num_traits::{One, Zero};
 use std::marker::PhantomData;
 
-pub struct Variables<T = f64> {
-    max_var: usize,
+pub struct Variables<T, const MAX_VAR: usize> {
     num_var: usize,
     _type: PhantomData<T>,
 }
 
-impl<T> Variables<T>
+impl<T, const MAX_VAR: usize> Variables<T, MAX_VAR>
 where
-    T: One + Zero + Clone,
+    T: One + Zero + Copy,
 {
-    pub fn new(num_variables: usize) -> Self {
+    pub fn new() -> Self {
         Self {
-            max_var: num_variables,
             num_var: 0,
             _type: PhantomData::<T>,
         }
     }
 
-    fn var(i: usize, n: usize, v: T) -> Dual<T> {
-        let mut dx = Array1::<T>::zeros(n);
+    fn var(i: usize, v: T) -> Dual<T, MAX_VAR> {
+        let mut dx = [T::zero(); MAX_VAR];
         dx[i] = T::one();
-        Dual::<T> {
-            x: v,
-            dx: ArcArray::from(dx),
-        }
+        Dual::<T, MAX_VAR> { x: v, dx: dx }
     }
 
-    pub fn gen(&mut self, init_value: T) -> Option<Dual<T>> {
-        if self.num_var < self.max_var {
-            let var = Self::var(self.num_var, self.max_var, init_value);
+    pub fn gen(&mut self, init_value: T) -> Option<Dual<T, MAX_VAR>> {
+        if self.num_var < MAX_VAR {
+            let var = Self::var(self.num_var, init_value);
             self.num_var += 1;
             Some(var)
         } else {
@@ -39,54 +34,67 @@ where
         }
     }
 
-    pub fn gen_all(&mut self, init_values: &[T]) -> Vec<Dual<T>> {
-        if init_values.len() == self.max_var - self.num_var {
+    pub fn gen_all(&mut self, init_values: &[T]) -> Vec<Dual<T, MAX_VAR>> {
+        if init_values.len() == MAX_VAR - self.num_var {
             init_values
                 .iter()
-                .zip(self.num_var..self.max_var)
-                .map(|(v, i)| Self::var(i, self.max_var, v.clone()))
+                .zip(self.num_var..MAX_VAR)
+                .map(|(v, i)| Self::var(i, v.clone()))
                 .collect()
         } else {
             panic!();
         }
     }
 
-    pub fn constant(&self, value: T) -> Dual<T> {
-        Dual::<T> {
+    pub fn constant(&self, value: T) -> Dual<T, MAX_VAR> {
+        Dual::<T, MAX_VAR> {
             x: value,
-            dx: ArcArray::from(Array1::<T>::zeros(self.max_var)),
+            dx: [T::zero(); MAX_VAR],
         }
     }
 }
 
 #[derive(Debug, Clone)]
-pub struct Dual<T = f64> {
+pub struct Dual<T, const N: usize> {
     x: T,
-    dx: ArcArray<T, Ix1>,
+    dx: [T; N],
 }
 
-impl<T> Dual<T>
-where
-    T: Clone,
-{
-    pub fn grad(&self) -> Option<ArrayView1<T>> {
-        Some(self.dx.view())
+impl<T, const N: usize> Dual<T, N> {
+    pub fn val(&self) -> &T {
+        &self.x
     }
 
-    pub fn grad_mut(&mut self) -> Option<ArrayViewMut1<T>> {
-        Some(self.dx.view_mut())
+    pub fn grad(&self) -> ArrayView1<T> {
+        ArrayView1::<T>::from(&self.dx)
+    }
+
+    pub fn grad_mut(&mut self) -> ArrayViewMut1<T> {
+        ArrayViewMut1::<T>::from(&mut self.dx)
+    }
+}
+
+impl<T, const N: usize> Default for Dual<T, N>
+where
+    T: Zero + Default + Copy,
+{
+    fn default() -> Self {
+        Self {
+            x: T::default(),
+            dx: [T::zero(); N],
+        }
     }
 }
 
 /*
-impl<T> From<Dual<T>> for T
+impl<T, const N: usize> From<Dual<T, N>> for T
 {
-    fn from(item: Dual<T>) -> T {
+    fn from(item: Dual<T, N>) -> T {
         item.x
     }
 }
 
-impl<T> Into<T> for Dual<T>
+impl<T, const N: usize> Into<T> for Dual<T, N>
 {
     fn into(self) -> T {
         self.x
@@ -94,33 +102,48 @@ impl<T> Into<T> for Dual<T>
 }
 */
 
-impl From<Dual<f64>> for f64 {
-    fn from(item: Dual<f64>) -> f64 {
+impl<const N: usize> From<Dual<f64, N>> for f64 {
+    fn from(item: Dual<f64, N>) -> f64 {
+        item.x
+    }
+}
+
+impl<const N: usize> From<&Dual<f64, N>> for f64 {
+    fn from(item: &Dual<f64, N>) -> f64 {
         item.x
     }
 }
 
 /*
-impl<T, U> From<Dual<T>> for Dual<U>
+impl<T, U, const N: usize> From<Dual<T, N>> for Dual<U, N>
 where
     U: From<T>,
+    [U; N]: Default,
 {
-    fn from(item: Dual<T>) -> Dual<U> {
-        Dual::<U> {
+    fn from(item: Dual<T, N>) -> Dual<U, N> {
+        let mut v: [U; N] = Default::default();
+        for (dst, src) in v.iter_mut().zip(item.dx) {
+            *dst = src.into();
+        }
+        Dual::<U, N> {
             x: U::from(item.x),
-            dx: item.dx.map(|&v| U::from(v)),
+            dx: v,
         }
     }
 }
 
-impl<T> From<Dual<T>> for Dual<f64>
+impl<T, const N: usize> From<Dual<T, N>> for Dual<f64, N>
 where
     T: Into<f64>,
 {
-    fn from(item: Dual<T>) -> Dual<f64> {
-        Dual::<f64> {
+    fn from(item: Dual<T, N>) -> Dual<f64, N> {
+        let mut v = [0.; N];
+        for (dst, src) in v.iter_mut().zip(item.dx){
+            *dst = src.into();
+        }
+        Dual::<f64, N> {
             x: item.x.into(),
-            dx: item.dx.map(|&v| v.into()),
+            dx: v,
         }
     }
 }
@@ -129,19 +152,82 @@ where
 mod ops;
 pub use self::ops::*;
 
+impl<T, const N: usize> Zero for Dual<T, N>
+where
+    T: Zero + Copy + PartialEq<T>,
+{
+    fn zero() -> Self {
+        Self {
+            x: T::zero(),
+            dx: [T::zero(); N],
+        }
+    }
+
+    fn is_zero(&self) -> bool {
+        self.x == T::zero()
+    }
+
+    fn set_zero(&mut self) {
+        self.x = T::zero();
+        for dst in self.dx.iter_mut() {
+            *dst = T::zero();
+        }
+    }
+}
+
+impl<T, const N: usize> One for Dual<T, N>
+where
+    T: Zero + One + PartialEq<T> + Copy,
+{
+    fn one() -> Self {
+        Self {
+            x: T::one(),
+            dx: [T::zero(); N],
+        }
+    }
+
+    fn set_one(&mut self) {
+        self.x = T::one();
+        for dst in self.dx.iter_mut() {
+            *dst = T::one();
+        }
+    }
+
+    fn is_one(&self) -> bool {
+        self.x == T::one()
+    }
+}
+
+use crate::traits::Extension;
+impl<const N: usize> Extension<f32> for Dual<f32, N> {}
+impl<const N: usize> Extension<f64> for Dual<f64, N> {}
+
+mod elementary;
+pub use self::elementary::*;
+
 #[cfg(test)]
 mod test {
     use super::*;
 
     #[test]
-    fn it_works() {
-        let mut vars = Variables::new(3);
+    fn case01() {
+        let mut vars = Variables::<f64, 3>::new();
         let x = vars.gen(0.).unwrap();
-        let rest = vars.gen_all(&[1., 10.]);
-        let y = rest[0].clone();
-        let z = rest[1].clone();
+        let y = vars.gen_all(&[1., 10.]);
 
-        let loss = (x + y) * z;
-        assert_eq!(loss.grad(), Some(array![10., 10., 1.].view()));
+        let loss = (&x + &y[0]) * &y[1];
+        assert_eq!(array![10., 10., 1.].view(), loss.grad());
+        assert_eq!((0. + 1.) * 10., *loss.val());
+    }
+
+    #[test]
+    fn case02() {
+        let mut vars = Variables::<f64, 3>::new();
+        let x = vars.gen(2.).unwrap();
+        let y = vars.gen_all(&[1., 10.]);
+
+        let loss = &x + (&x + &y[0]) * &y[1];
+        assert_eq!(array![11., 10., 3.].view(), loss.grad());
+        assert_eq!(2. + (2. + 1.) * 10., *loss.val());
     }
 }
--- src/dual/ops.rs
+++ src/dual/ops.rs
@@ -1,12 +1,10 @@
-use ndarray::prelude::*;
-use ndarray::ScalarOperand;
 use num_traits::{One, Zero};
 use std::cmp::Ordering;
 use std::ops::{Add, Div, Mul, Neg, Sub};
 
 use super::Dual;
 
-impl<T> PartialEq<Self> for Dual<T>
+impl<T, const N: usize> PartialEq<Self> for Dual<T, N>
 where
     T: PartialEq,
 {
@@ -19,7 +17,7 @@ where
     }
 }
 
-impl<T> PartialEq<T> for Dual<T>
+impl<T, const N: usize> PartialEq<T> for Dual<T, N>
 where
     T: PartialEq,
 {
@@ -32,7 +30,7 @@ where
     }
 }
 
-impl<T> PartialOrd<Self> for Dual<T>
+impl<T, const N: usize> PartialOrd<Self> for Dual<T, N>
 where
     T: PartialOrd,
 {
@@ -41,7 +39,7 @@ where
     }
 }
 
-impl<T> PartialOrd<T> for Dual<T>
+impl<T, const N: usize> PartialOrd<T> for Dual<T, N>
 where
     T: PartialOrd,
 {
@@ -50,123 +48,308 @@ where
     }
 }
 
-impl<T> Add<Self> for Dual<T>
+impl<T, const N: usize> Add<&Self> for Dual<T, N>
 where
-    T: Zero + Add + Clone,
+    T: Zero + Add + Copy,
 {
-    type Output = Self;
+    type Output = Dual<T, N>;
+    fn add(mut self, rhs: &Self) -> Self::Output {
+        for (dst, &src) in self.dx.iter_mut().zip(rhs.dx.iter()) {
+            *dst = *dst + src;
+        }
+        self.x = self.x + rhs.x;
+        self
+    }
+}
+
+impl<T, const N: usize> Add<Self> for Dual<T, N>
+where
+    T: Zero + Add + Copy,
+{
+    type Output = Dual<T, N>;
+    #[inline]
     fn add(self, rhs: Self) -> Self::Output {
-        Self {
-            x: self.x + rhs.x,
-            dx: ArcArray::from(&self.dx + &rhs.dx),
-        }
+        self + &rhs
     }
 }
 
-impl<T> Add<T> for Dual<T>
+impl<'a, 'b, T, const N: usize> Add<&'a Dual<T, N>> for &'b Dual<T, N>
 where
-    T: Zero + Add + Clone,
+    T: Zero + Add + Copy,
 {
-    type Output = Self;
+    type Output = Dual<T, N>;
+    #[inline]
+    fn add(self, rhs: &'a Dual<T, N>) -> Self::Output {
+        self.clone() + rhs
+    }
+}
+
+impl<T, const N: usize> Add<Dual<T, N>> for &Dual<T, N>
+where
+    T: Zero + Add + Copy,
+{
+    type Output = Dual<T, N>;
+    #[inline]
+    fn add(self, rhs: Dual<T, N>) -> Self::Output {
+        rhs + self
+    }
+}
+
+impl<T, const N: usize> Add<T> for Dual<T, N>
+where
+    T: Add<Output = T> + Copy,
+{
+    type Output = Dual<T, N>;
+    fn add(mut self, rhs: T) -> Self::Output {
+        self.x = self.x + rhs;
+        self
+    }
+}
+
+impl<T, const N: usize> Add<T> for &Dual<T, N>
+where
+    T: Add<Output = T> + Copy,
+{
+    type Output = Dual<T, N>;
+    #[inline]
     fn add(self, rhs: T) -> Self::Output {
-        Self {
-            x: self.x + rhs,
-            dx: self.dx,
-        }
+        self.clone() + rhs
     }
 }
 
-impl<T> Sub<Self> for Dual<T>
+impl<T, const N: usize> Neg for Dual<T, N>
 where
-    T: Zero + Sub<Output = T> + Clone,
+    T: Zero + Neg<Output = T> + Sub<Output = T> + Copy,
 {
     type Output = Self;
-    fn sub(self, rhs: Self) -> Self::Output {
-        Self {
-            x: self.x - rhs.x,
-            dx: ArcArray::from(&self.dx - &rhs.dx),
+    fn neg(mut self) -> Self::Output {
+        for dst in self.dx.iter_mut() {
+            *dst = -*dst;
         }
+        self.x = -self.x;
+        self
     }
 }
 
-impl<T> Sub<T> for Dual<T>
+impl<T, const N: usize> Neg for &Dual<T, N>
 where
-    T: Zero + Sub<Output = T> + Clone,
+    T: Zero + Neg<Output = T> + Sub<Output = T> + Copy,
 {
-    type Output = Self;
-    fn sub(self, rhs: T) -> Self::Output {
-        Self {
-            x: self.x - rhs,
-            dx: self.dx,
-        }
-    }
-}
-
-impl<T> Neg for Dual<T>
-where
-    T: Zero + Neg<Output = T> + Sub<Output = T> + Clone,
-{
-    type Output = Self;
+    type Output = Dual<T, N>;
     fn neg(self) -> Self::Output {
-        Self {
-            x: -self.x,
-            dx: ArcArray::from(-self.dx.to_owned()),
-        }
+        -self.clone()
     }
 }
 
-impl<T> Mul<Self> for Dual<T>
+impl<T, const N: usize> Sub<&Self> for Dual<T, N>
 where
-    T: Zero + One + Add + Mul + ScalarOperand + Clone,
+    T: Zero + Sub<Output = T> + Copy,
 {
     type Output = Self;
+    fn sub(mut self, rhs: &Self) -> Self::Output {
+        for (dst, &src) in self.dx.iter_mut().zip(rhs.dx.iter()) {
+            *dst = *dst - src;
+        }
+        self.x = self.x - rhs.x;
+        self
+    }
+}
+
+impl<T, const N: usize> Sub<Self> for Dual<T, N>
+where
+    T: Zero + Sub<Output = T> + Copy,
+{
+    type Output = Self;
+    #[inline]
+    fn sub(self, rhs: Self) -> Self::Output {
+        self - &rhs
+    }
+}
+
+impl<'a, 'b, T, const N: usize> Sub<&'a Dual<T, N>> for &'b Dual<T, N>
+where
+    T: Zero + Sub<Output = T> + Copy,
+{
+    type Output = Dual<T, N>;
+    #[inline]
+    fn sub(self, rhs: &'a Dual<T, N>) -> Self::Output {
+        self.clone() - rhs
+    }
+}
+
+impl<T, const N: usize> Sub<Dual<T, N>> for &Dual<T, N>
+where
+    T: Zero + Sub<Output = T> + Copy,
+{
+    type Output = Dual<T, N>;
+    #[inline]
+    fn sub(self, rhs: Dual<T, N>) -> Self::Output {
+        self.clone() - &rhs
+    }
+}
+
+impl<T, const N: usize> Sub<T> for Dual<T, N>
+where
+    T: Zero + Sub<Output = T> + Copy,
+{
+    type Output = Self;
+    fn sub(mut self, rhs: T) -> Self::Output {
+        self.x = self.x - rhs;
+        self
+    }
+}
+
+impl<T, const N: usize> Sub<T> for &Dual<T, N>
+where
+    T: Zero + Sub<Output = T> + Copy,
+{
+    type Output = Dual<T, N>;
+    #[inline]
+    fn sub(self, rhs: T) -> Self::Output {
+        self.clone() - rhs
+    }
+}
+
+impl<T, const N: usize> Mul<&Self> for Dual<T, N>
+where
+    T: Zero + One + Add + Mul + Copy,
+{
+    type Output = Dual<T, N>;
+    fn mul(mut self, rhs: &Self) -> Self::Output {
+        for (dst, &src) in self.dx.iter_mut().zip(rhs.dx.iter()) {
+            *dst = *dst * rhs.x + src * self.x;
+        }
+        self.x = self.x * rhs.x;
+        self
+    }
+}
+
+impl<T, const N: usize> Mul<Self> for Dual<T, N>
+where
+    T: Zero + One + Add + Mul + Copy,
+{
+    type Output = Dual<T, N>;
+    #[inline]
     fn mul(self, rhs: Self) -> Self::Output {
-        Self {
-            x: self.x.clone() * rhs.x.clone(),
-            dx: ArcArray::from(&self.dx * rhs.x + &rhs.dx * self.x),
-        }
+        self * &rhs
     }
 }
 
-impl<T> Mul<T> for Dual<T>
+impl<'a, 'b, T, const N: usize> Mul<&'a Dual<T, N>> for &'b Dual<T, N>
 where
-    T: Zero + One + Add + Mul + ScalarOperand + Clone,
+    T: Zero + One + Add + Mul + Copy,
 {
-    type Output = Self;
+    type Output = Dual<T, N>;
+    #[inline]
+    fn mul(self, rhs: &'a Dual<T, N>) -> Self::Output {
+        self.clone() * rhs
+    }
+}
+
+impl<T, const N: usize> Mul<Dual<T, N>> for &Dual<T, N>
+where
+    T: Zero + One + Add + Mul + Copy,
+{
+    type Output = Dual<T, N>;
+    #[inline]
+    fn mul(self, rhs: Dual<T, N>) -> Self::Output {
+        rhs * self
+    }
+}
+
+impl<T, const N: usize> Mul<T> for Dual<T, N>
+where
+    T: Zero + One + Add + Mul + Copy,
+{
+    type Output = Dual<T, N>;
+    fn mul(mut self, rhs: T) -> Self::Output {
+        for dst in self.dx.iter_mut() {
+            *dst = *dst * rhs;
+        }
+        self.x = self.x * rhs;
+        self
+    }
+}
+
+impl<T, const N: usize> Mul<T> for &Dual<T, N>
+where
+    T: Zero + One + Add + Mul + Copy,
+{
+    type Output = Dual<T, N>;
+    #[inline]
     fn mul(self, rhs: T) -> Self::Output {
-        Self {
-            x: self.x * rhs.clone(),
-            dx: ArcArray::from(&self.dx * rhs),
-        }
+        self.clone() * rhs
     }
 }
 
-impl<T> Div<Self> for Dual<T>
+impl<T, const N: usize> Div<&Self> for Dual<T, N>
 where
-    T: Zero + One + Add + Sub<Output = T> + Mul + Div<Output = T> + ScalarOperand + Clone,
+    T: Zero + One + Add + Sub<Output = T> + Mul + Div<Output = T> + Copy,
 {
-    type Output = Self;
+    type Output = Dual<T, N>;
+    fn div(mut self, rhs: &Self) -> Self::Output {
+        for (dst, src) in self.dx.iter_mut().zip(rhs.dx) {
+            *dst = *dst * rhs.x - src * self.x / (rhs.x * rhs.x);
+        }
+        self.x = self.x / rhs.x;
+        self
+    }
+}
+
+impl<T, const N: usize> Div<Self> for Dual<T, N>
+where
+    T: Zero + One + Add + Sub<Output = T> + Mul + Div<Output = T> + Copy,
+{
+    type Output = Dual<T, N>;
+    #[inline]
     fn div(self, rhs: Self) -> Self::Output {
-        Self {
-            dx: ArcArray::from(
-                &self.dx * rhs.x.clone()
-                    - &rhs.dx * self.x.clone() / (rhs.x.clone() * rhs.x.clone()),
-            ),
-            x: self.x / rhs.x,
-        }
+        self / &rhs
     }
 }
 
-impl<T> Div<T> for Dual<T>
+impl<'a, 'b, T, const N: usize> Div<&'a Dual<T, N>> for &'b Dual<T, N>
 where
-    T: Zero + One + Add + Sub<Output = T> + Mul + Div<Output = T> + ScalarOperand + Clone,
+    T: Zero + One + Add + Sub<Output = T> + Mul + Div<Output = T> + Copy,
 {
-    type Output = Self;
-    fn div(self, rhs: T) -> Self::Output {
-        Self {
-            dx: ArcArray::from(&self.dx / rhs.clone()),
-            x: self.x / rhs,
-        }
+    type Output = Dual<T, N>;
+    #[inline]
+    fn div(self, rhs: &'a Dual<T, N>) -> Self::Output {
+        self.clone() / rhs
     }
 }
 
+impl<T, const N: usize> Div<Dual<T, N>> for &Dual<T, N>
+where
+    T: Zero + One + Add + Sub<Output = T> + Mul + Div<Output = T> + Copy,
+{
+    type Output = Dual<T, N>;
+    #[inline]
+    fn div(self, rhs: Dual<T, N>) -> Self::Output {
+        self.clone() / &rhs
+    }
+}
+
+impl<T, const N: usize> Div<T> for Dual<T, N>
+where
+    T: Zero + One + Add + Sub<Output = T> + Mul + Div<Output = T> + Copy,
+{
+    type Output = Dual<T, N>;
+    fn div(mut self, rhs: T) -> Self::Output {
+        for dst in self.dx.iter_mut() {
+            *dst = *dst / rhs;
+        }
+        self.x = self.x / rhs;
+        self
+    }
+}
+
+impl<T, const N: usize> Div<T> for &Dual<T, N>
+where
+    T: Zero + One + Add + Sub<Output = T> + Mul + Div<Output = T> + Copy,
+{
+    type Output = Dual<T, N>;
+    #[inline]
+    fn div(self, rhs: T) -> Self::Output {
+        self.clone() / rhs
+    }
+}
