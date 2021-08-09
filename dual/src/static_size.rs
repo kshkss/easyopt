0b1690e8a9234cf4948b884ac92514584efe0886
Akishika Issui <kshkss@users.noreply.github.com>
2021-08-09 21:03:27 +0900

    Remove the builder for Dual

--- dual/src/static_size.rs
+++ dual/src/static_size.rs
@@ -1,58 +1,5 @@
 use ndarray::prelude::*;
 use num_traits::{One, Zero};
-use std::marker::PhantomData;
-
-pub struct Variables<T, const MAX_VAR: usize> {
-    num_var: usize,
-    _type: PhantomData<T>,
-}
-
-impl<T, const MAX_VAR: usize> Variables<T, MAX_VAR>
-where
-    T: One + Zero + Copy,
-{
-    pub fn new() -> Self {
-        Self {
-            num_var: 0,
-            _type: PhantomData::<T>,
-        }
-    }
-
-    fn var(i: usize, v: T) -> Dual<T, MAX_VAR> {
-        let mut dx = [T::zero(); MAX_VAR];
-        dx[i] = T::one();
-        Dual::<T, MAX_VAR> { x: v, dx: dx }
-    }
-
-    pub fn gen(&mut self, init_value: T) -> Option<Dual<T, MAX_VAR>> {
-        if self.num_var < MAX_VAR {
-            let var = Self::var(self.num_var, init_value);
-            self.num_var += 1;
-            Some(var)
-        } else {
-            None
-        }
-    }
-
-    pub fn gen_all(&mut self, init_values: &[T]) -> Vec<Dual<T, MAX_VAR>> {
-        if init_values.len() == MAX_VAR - self.num_var {
-            init_values
-                .iter()
-                .zip(self.num_var..MAX_VAR)
-                .map(|(v, i)| Self::var(i, v.clone()))
-                .collect()
-        } else {
-            panic!();
-        }
-    }
-
-    pub fn constant(&self, value: T) -> Dual<T, MAX_VAR> {
-        Dual::<T, MAX_VAR> {
-            x: value,
-            dx: [T::zero(); MAX_VAR],
-        }
-    }
-}
 
 #[derive(Debug, Clone)]
 pub struct Dual<T, const N: usize> {
@@ -60,7 +7,27 @@ pub struct Dual<T, const N: usize> {
     dx: [T; N],
 }
 
-impl<T, const N: usize> Dual<T, N> {
+impl<T, const N: usize> Dual<T, N>
+where
+    T: Copy + Zero + One,
+{
+    pub fn new(var_index: usize, value: T) -> Self {
+        assert!(var_index < N);
+        let mut v = Self {
+            x: value,
+            dx: [T::zero(); N],
+        };
+        v.dx[var_index] = T::one();
+        v
+    }
+
+    pub fn constant(value: T) -> Self {
+        Self {
+            x: value,
+            dx: [T::zero(); N],
+        }
+    }
+
     pub fn val(&self) -> &T {
         &self.x
     }
@@ -204,25 +171,26 @@ mod test {
     use super::*;
 
     #[test]
-    fn case01() {
-        let mut vars = Variables::<f64, 3>::new();
-        let x = vars.gen(0.).unwrap();
-        let y = vars.gen_all(&[1., 10.]);
+    fn case01() -> anyhow::Result<()> {
+        let x = Dual::<f64, 3>::new(0, 0.);
+        let y = [Dual::<f64, 3>::new(1, 1.), Dual::<f64, 3>::new(2, 10.)];
 
         let loss = (&x + &y[0]) * &y[1];
         assert_eq!(array![10., 10., 1.].view(), loss.grad());
         assert_eq!((0. + 1.) * 10., *loss.val());
+
+        Ok(())
     }
 
     #[test]
-    fn case02() {
-        let mut vars = Variables::<f64, 3>::new();
-        let x = vars.gen(2.).unwrap();
-        let y = vars.gen_all(&[1., 10.]);
+    fn case02() -> anyhow::Result<()> {
+        let x = Dual::<f64, 3>::new(0, 2.);
+        let y = [Dual::<f64, 3>::new(1, 1.), Dual::<f64, 3>::new(2, 10.)];
 
         let loss = &x + (&x + &y[0]) * &y[1];
         assert_eq!(array![11., 10., 3.].view(), loss.grad());
         assert_eq!(2. + (2. + 1.) * 10., *loss.val());
+
+        Ok(())
     }
 }
-
